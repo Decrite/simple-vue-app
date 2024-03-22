3d6e0aed72c28a9b5cbad7b76aaee84fb0f2a951
Decrite <a309.alex@gmail.com>
2024-03-22 20:46:16 +0100

    feat: cleaned up a bit

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,26 @@
 <template>
-
-  <div class="camera-container">
-    <CameraView @imageCaptured="addImage" />
-  </div>
-  <div class="gallery-container">
-    <ImageGallery :images="images" />
-  </div>
-
+  <CameraView @imageCaptured="addImage" />
+  <ImageGallery :images="images" />
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import CameraView from './components/CameraView.vue';
-import ImageGallery from './components/ImageGallery.vue';
+import { defineComponent, ref } from 'vue'
+import CameraView from './components/CameraView.vue'
+import ImageGallery from './components/ImageGallery.vue'
 
 export default defineComponent({
   components: {
     CameraView,
-    ImageGallery,
+    ImageGallery
   },
   setup() {
-    const images = ref<string[]>([]);
+    const images = ref<string[]>([])
 
     const addImage = (imageSrc: string) => {
-      images.value.push(imageSrc);
-    };
+      images.value.push(imageSrc)
+    }
 
-    return { images, addImage };
-  },
-});
+    return { images, addImage }
+  }
+})
 </script>
-<style>
-/* Gesamtes Layout */
-.camera-container,
-.gallery-container {
-  height: 100vh;
-  /* Stellt sicher, dass beide Container die volle Höhe des Viewports einnehmen */
-}
-
-.camera-container {
-  float: left;
-  width: 100vb;
-  height: 100vh;
-  overflow: hidden;
-}
-
-.gallery-container {
-  float: right;
-  width: 100vb;
-  height: 100vh;
-  overflow-y: auto;
-}
-
-@media (max-width: 768px) {
-
-  .camera-container,
-  .gallery-container {
-    width: 100%;
-    height: 50vh;
-    float: none;
-  }
-
-  .gallery-container {
-    overflow-y: auto;
-  }
-}
-</style>
