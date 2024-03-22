e0e062fd98bfa38e2184287e89c6b2e8643bb67a
Florian Groß <Magic_LP@web.de>
2024-03-22 15:47:43 +0100

    Fixed Position of the Buttons & Removed Carusel Button

--- src/components/CameraView.vue
+++ src/components/CameraView.vue
@@ -1,37 +1,16 @@
 <template>
   <header class="header">MyPhotoGallery</header>
   <div class="camera-container">
-    <camera
-      v-if="!isMobile"
-      :resolution="{ width: 375, height: 812 }"
-      ref="camera"
-      autoplay
-    ></camera>
-    <input
-      v-else
-      type="file"
-      name="image"
-      accept="image/*"
-      capture="user"
-      @change="handleImageInput"
-      ref="fileInput"
-      style="display: none"
-    />
+    <camera v-if="!isMobile" :resolution="{ width: 375, height: 812 }" ref="camera" autoplay></camera>
+    <input v-else type="file" name="image" accept="image/*" capture="user" @change="handleImageInput" ref="fileInput"
+      style="display: none" />
     <button v-if="!isMobile" class="round-button toggle-button" @click="toggleCamera"></button>
     <button v-if="!isMobile" class="round-button snapshot-button" @click="snapshot"></button>
     <div class="phone">
-      <svg
-        v-if="isMobile"
-        class="camera-icon"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        @click="triggerFileInput"
-        width="70"
-        height="64"
-      >
+      <svg v-if="isMobile" class="camera-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+        @click="triggerFileInput" width="70" height="64">
         <path
-          d="M20 7h-1.26c-.45-1.45-1.58-2.58-3.03-3.03V2c0-.55-.45-1-1-1H9c-.55 0-1 .45-1 1v1.97C6.84 4.42 5.71 5.55 5.26 7H4c-.55 0-1 .45-1 1v13c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V8c0-.55-.45-1-1-1zM12 18c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z"
-        />
+          d="M20 7h-1.26c-.45-1.45-1.58-2.58-3.03-3.03V2c0-.55-.45-1-1-1H9c-.55 0-1 .45-1 1v1.97C6.84 4.42 5.71 5.55 5.26 7H4c-.55 0-1 .45-1 1v13c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V8c0-.55-.45-1-1-1zM12 18c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z" />
       </svg>
     </div>
   </div>
@@ -123,6 +102,7 @@ export default defineComponent({
   transform: translateX(-75%);
   width: 50px;
   height: 50px;
+  bottom: 60px;
 }
 
 .toggle-button {
@@ -130,6 +110,7 @@ export default defineComponent({
   transform: translateX(25%);
   width: 50px;
   height: 50px;
+  bottom: 60px;
 }
 
 .phone {
@@ -140,7 +121,8 @@ export default defineComponent({
 }
 
 .phone .camera-icon path {
-  fill: white; /* Change color to white */
+  fill: white;
+  /* Change color to white */
 }
 
 .header {
