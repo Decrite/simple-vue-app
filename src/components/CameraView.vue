dfdea0c3e103ee3bea581851a1b3d119c42a6d8c
Decrite <a309.alex@gmail.com>
2024-03-23 15:16:49 +0100

    .

--- src/components/CameraView.vue
+++ src/components/CameraView.vue
@@ -5,13 +5,29 @@
     <button v-if="!isMobile" class="round-button centerButton" @click="snapshot"></button>
     <button v-if="!isMobile" class="round-button centerButton" @click="toggleCamera"></button>
 
-    <input v-else type="file" name="image" accept="image/*" capture="user" @change="handleImageInput" ref="fileInput"
-      style="display: none" />
+    <input
+      v-else
+      type="file"
+      name="image"
+      accept="image/*"
+      capture="user"
+      @change="handleImageInput"
+      ref="fileInput"
+      style="display: none"
+    />
     <div class="phone">
-      <svg v-if="isMobile" class="camera-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-        @click="triggerFileInput" width="70" height="64">
+      <svg
+        v-if="isMobile"
+        class="camera-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        @click="triggerFileInput"
+        width="70"
+        height="64"
+      >
         <path
-          d="M20 7h-1.26c-.45-1.45-1.58-2.58-3.03-3.03V2c0-.55-.45-1-1-1H9c-.55 0-1 .45-1 1v1.97C6.84 4.42 5.71 5.55 5.26 7H4c-.55 0-1 .45-1 1v13c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V8c0-.55-.45-1-1-1zM12 18c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z" />
+          d="M20 7h-1.26c-.45-1.45-1.58-2.58-3.03-3.03V2c0-.55-.45-1-1-1H9c-.55 0-1 .45-1 1v1.97C6.84 4.42 5.71 5.55 5.26 7H4c-.55 0-1 .45-1 1v13c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V8c0-.55-.45-1-1-1zM12 18c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z"
+        />
       </svg>
     </div>
   </div>
@@ -57,8 +73,7 @@ export default defineComponent({
 
     const handleImageInput = (event: any) => {
       const file = event.target.files[0]
-      const url = URL.createObjectURL(file)
-      emit('imageCaptured', url)
+      emit('imageCaptured', file)
     }
 
     const triggerFileInput = () => {
@@ -90,7 +105,6 @@ export default defineComponent({
 
 .camera-container {
   position: relative;
-  display: inline-block;
 }
 
 .round-button {
@@ -125,6 +139,7 @@ export default defineComponent({
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100%;
 }
 
 .phone .camera-icon path {
